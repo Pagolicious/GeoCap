c1cbb941e2e1007a52cc37386a14598d48d7e4a2
AlexanderOlofsson <alex.j.olofsson@gmail.com>
2024-02-21 18:31:17 +0100

    HelpPowerup
    How do I implement Powerup.vue into CategoryQuizView.vue while getting the array data from FetchQuestions.vue?
    
    (using 50/50 should disable two values  from one of the arrays inside of FetchQuestions.vue)

---
+++ src/components/Powerup.vue
@@ -0,0 +1,170 @@
+<script setup>
+import { ref } from 'vue'
+
+// Track if fiftyfifty is used
+let fiftyFiftyDisabled = ref(false)
+
+
+// Function to activate fiftyfifty
+const activateFiftyFifty = () => {
+  if (!fiftyFiftyDisabled.value) { // Checking if powerup is used or not
+    let disabledBtns = 0;
+    let disabledPicks = [];
+    // Pick two random indexes to disable
+    while (disabledBtns < 2) {
+      const index = Math.floor(Math.random() * 3); // Picks a random index between 0 and 2
+      if (!disabledPicks.includes(index)) { // Checks if the index is unique
+        disabledPicks.push(index); // Adds the index to the disabledpicks array
+        disabledBtns++; // ++ the amount of disabled buttons
+      }
+    }
+    // Disabled picked buttons and update
+    for (let i = 0; i < disabledPicks.length; i++) {
+      incorrect[disabledPicks[i]] = '';
+    }
+    // Turns fiftyFiftyDisabled into true > this disables the power-up
+    fiftyFiftyDisabled.value = true;
+    console.log("incorrect array after powerup", incorrect);
+  }
+}
+console.log("incorrect array before powerup", incorrect);
+</script>
+
+
+<template>
+  <div class="powerUps">
+    <button class="powerBtn" :class="{ 'disabled': fiftyFiftyDisabled }" id="fiftyFifty" @click="activateFiftyFifty">50/50</button>
+    <button class="powerBtn" id="shield">SHIELD</button>
+    <button class="powerBtn" id="pass">PASS</button>
+  </div>
+</template>
+
+
+<!-- HOW ORIGINAL FILE-SETUP LOOKED LIKE -->
+
+<!-- <script setup>
+import { ref } from 'vue'
+
+let incorrect = ["Moscow", "Paris", "Oslo"]
+let correct = "Stockholm"
+
+// Function to shuffle the array
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+// Shuffle the incorrect array
+shuffleArray(incorrect)
+
+
+// Track if fiftyfifty is used
+let fiftyFiftyDisabled = ref(false)
+
+
+// Function to activate fiftyfifty
+const activateFiftyFifty = () => {
+  if (!fiftyFiftyDisabled.value) { // Checking if powerup is used or not
+    let disabledBtns = 0;
+    let disabledPicks = [];
+    // Pick two random indexes to disable
+    while (disabledBtns < 2) {
+      const index = Math.floor(Math.random() * 3); // Picks a random index between 0 and 2
+      if (!disabledPicks.includes(index)) { // Checks if the index is unique
+        disabledPicks.push(index); // Adds the index to the disabledpicks array
+        disabledBtns++; // ++ the amount of disabled buttons
+      }
+    }
+    // Disabled picked buttons and update
+    for (let i = 0; i < disabledPicks.length; i++) {
+      incorrect[disabledPicks[i]] = '';
+    }
+    // Turns fiftyFiftyDisabled into true > this disables the power-up
+    fiftyFiftyDisabled.value = true;
+    console.log("incorrect array after powerup", incorrect);
+  }
+}
+console.log("incorrect array before powerup", incorrect);
+</script>
+
+<template>
+  <h1>Which capital?</h1>
+  <div class="answer">
+    <button class="quizButton" :class="{ 'disabled': fiftyFiftyDisabled && incorrect[0] === '' }">{{ incorrect[0] }}</button>
+  </div>
+  <div class="answer">
+    <button class="quizButton" :class="{ 'disabled': fiftyFiftyDisabled && incorrect[1] === '' }">{{ incorrect[1] }}</button>
+  </div>
+  <div class="answer">
+    <button class="quizButton" :class="{ 'disabled': fiftyFiftyDisabled && incorrect[2] === '' }">{{ incorrect[2] }}</button>
+  </div>
+  <div class="answer">
+    <button class="quizButton">{{ correct }}</button>
+  </div>
+
+  <div class="powerUps">
+    <button class="powerBtn" :class="{ 'disabled': fiftyFiftyDisabled }" id="fiftyFifty" @click="activateFiftyFifty">50/50</button>
+    <button class="powerBtn" id="shield">SHIELD</button>
+    <button class="powerBtn" id="pass">PASS</button>
+  </div>
+</template>
+
+<style scoped>
+.answerBtn.disabled {
+  background-color: rgb(97, 97, 97);
+}
+
+.answer {
+  height: 100px;
+  width: 400px;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  margin: 5px;
+  border-radius: 10px;
+}
+
+.quizButton {
+  background-color: sandybrown;
+  width: 100%;
+  height: 100%;
+}
+
+p {
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  font-weight: 1000;
+}
+
+.powerUps {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 4rem;
+}
+
+.powerBtn {
+  background-color: rgb(0, 146, 37);
+  color: rgb(6, 8, 1);
+  width: 59px;
+  height: 59px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+
+#fiftyFifty {
+  background-color: #3AB296;
+}
+
+#fiftyFifty.disabled {
+  background-color: gray;
+}
+</style> -->
--- src/views/CategoryQuizView.vue
+++ src/views/CategoryQuizView.vue
@@ -1,12 +1,85 @@
 <script setup>
 import Category from '../components/Category.vue'
 import FetchQuestions from '../components/FetchQuestions.vue'
+import Powerup from '../components/Powerup.vue';
 </script>
 
 <template>
   <Category />
-  <div>
+  <div id="container">
+    <div id="stickyQuiz">
     <FetchQuestions />
+    <h1>What is the capital<br>of this country?</h1>
+    <!-- flag -->
+    <div class="answer">
+      <button class="quizButton"><p id="quizP">Placeholder</p></button></div>
+    <div class="answer">
+      <button class="quizButton"><p id="quizP">Placeholder</p></button></div>
+    <div class="answer">
+    <button class="quizButton"><p id="quizP">Placeholder</p></button></div>
+    <div class="answer">
+      <button class="quizButton"><p id="quizP">Placeholder</p></button></div>
+   <!-- <Powerup /> -->
   </div>
+</div>
   <!-- <h1>Quiz</h1> -->
 </template>
+<style scoped>
+
+#container {
+  width: 100%;
+  background-color: F5F5F5;
+}
+
+#stickyQuiz {
+  position: sticky;
+  top: 25%;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+}
+
+.answer {
+  height: 5rem;
+  width: 20rem;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  margin: 0.4rem;
+}
+
+.quizButton {
+  width: 100%;
+  height: 100%;
+  border-radius: 0.4375rem;
+  border: 1px solid #E0E1E1;
+  background: #F5F5F5;
+  box-shadow: 0px 2px 2px 0px #E4E6E7;
+}
+.quizButton:hover {
+  border-color: #646cff;
+}
+.quizButton:focus,
+.quizButton:focus-visible {
+  outline: 4px auto -webkit-focus-ring-color;
+}
+
+#quizP {
+color: #0B0957;
+font-family: Montserrat;
+font-size: 1.2rem;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+margin-bottom: 0rem;
+}
+
+h1 {
+  font-family: "Fredoka", sans-serif;
+  font-size: 2.2em;
+  line-height: 1.1;
+  margin-bottom: 3rem;
+}
+</style>
